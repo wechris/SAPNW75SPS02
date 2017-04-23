00e4d04fe4ad15dfab81d1c5cf87ed709d8a1c0a
wechris <weiss_christian@web.de>
2017-04-23 20:29:41 +0200

    switch to Oracel Jvava

--- install/gnome.sh
+++ install/gnome.sh
@@ -8,3 +8,11 @@ zypper --non-interactive install --force-resolution --no-confirm --auto-agree-wi
 sudo sed -i 's/"sddm"/"gdm"/g' /etc/sysconfig/displaymanager
 sudo echo "/usr/bin/Xorg                 root:root       4711" >> /etc/permissions.local
 sudo chkstat --system --set
+
+echo "install JAVA..."
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-x64.rpm
+sudo zypper --non-interactive remove --force-resolution java
+sudo ln -s /usr/sbin/update-alternatives /usr/sbin/alternatives
+sudo rpm -ivh --nodeps jdk-8u101-linux-x64.rpm
+sudo update-alternatives --set java /usr/java/jdk1.8.0_101/jre/bin/java
+rm jdk-8u101-linux-x64.rpm
