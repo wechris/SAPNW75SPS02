34c8c965692f30d9054dc73136d79e93964b0a25
wechris <weiss_christian@web.de>
2018-01-14 17:00:49 +0100

    fixed java version

--- install/gnome.sh
+++ install/gnome.sh
@@ -14,7 +14,7 @@ echo "install JAVA..."
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.rpm
 sudo zypper --non-interactive remove --force-resolution java
 #sudo ln -s /usr/sbin/update-alternatives /usr/sbin/alternatives
-sudo zypper --non-interactive in --auto-agree-with-licenses /home/vagrant/jdk-8u66-linux-x64.rpm
+sudo zypper --non-interactive in --auto-agree-with-licenses /home/vagrant/jdk-8u152-linux-x64.rpm
 #sudo rpm -ivh --nodeps jdk-8u152-linux-x64.rpm
 #sudo update-alternatives --set java /usr/java/jdk1.8.0_152/jre/bin/java
 #rm jdk-8u152-linux-x64.rpm
